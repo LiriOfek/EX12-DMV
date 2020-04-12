152ed92cd35934a1549bd4ad6302939bad84bda0
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-12 10:58:14 +0300

    add function that check if person is old enough to drive

--- filter_drivers/main.c
+++ filter_drivers/main.c
@@ -4,3 +4,34 @@ Author: Liri (12/4/2020)
 Purpose: This file run the program that check which person
 		 is leagal to drive
 \********************************************************/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_old_enough_to_drive(int age,
+	int limit_age);
+
+bool is_old_enough_to_drive(int age, int limit_age) {
+	/********************************************************\
+	* Function name - is_old_enough_to_drive
+	*
+	* Function Purpose - check if the person is old enough to drive
+	*
+	* Parameters - IN int age - the age of the person,
+	*			   IN int limit_age - the smallest age for driving
+	*
+	* Return Value - if the person is old enough to drive - return true,
+	*				  otherwise - return false
+	*
+	* Side Effects - this function has no side effects
+	*
+	* Semantics - this function check if the age of the person is bigger than
+	*		      the limit age
+	*
+	* Author - Liri
+	\********************************************************/
+	if (age < limit_age) {
+		return false;
+	}
+	return true;
+}
