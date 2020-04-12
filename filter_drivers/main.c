0fa6fc142ce08c3f3bb88dcf49169b4b0b893a56
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-12 10:56:38 +0300

    add main file with documentation

---
+++ filter_drivers/main.c
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (12/4/2020)
+Purpose: This file run the program that check which person
+		 is leagal to drive
+\********************************************************/
